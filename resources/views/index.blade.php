da387e0dd315464f995da294f1a05ad9b5761498
Ismael <ismaelsantos@hotmail.com>
2022-02-25 15:09:03 -0400

    Habilitar carpetas ignoradas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.template')
 
+@extends('layouts.topbar')
+
 <body class="antialiased">
      
+    <section class="top-0">
+           
+           @yield('content')
+        
+   </section>
+
+      
      
 </body>
 
