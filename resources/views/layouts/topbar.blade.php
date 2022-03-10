de61c9561f9059a413bb3b567a040f40b4296c63
Ismael <ismaelsantos@hotmail.com>
2022-03-10 09:29:12 -0400

    Procesos de Envabezados

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -1,41 +1,33 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">UTESUR</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">ADMISIONES</a>
-          </li>
+
+<div style ="background-color: #1897EB; height: 40px;">
+    <div class ="container d-flex justify-content-between">
+         <row  class="p-2">
+          <a href="tel:+18095213785" id="TextBar01" class="d-flex align-items-center"><i class="fa-solid fa-phone youricons"></i> Tel: 809-521-3785</a>
+         </row>
+         
+         
+  
+         <div class="d-flex justify-content-around align-items-center" style="width :250px" > 
+       
+          <a href="#" id="TextBar01"><i class="fa-solid fa-user myicons"></i> Contactos </a> 
+          <a href=""><i class="d-block fa-brands fa-facebook-f myicons"></i></a>
+          <a href=""><i class="d-block fa-brands fa-instagram myicons"></i></a>
+          <a href=""><i class="d-block fa-brands fa-youtube myicons"></i></a>
+       
           
-          <li class="nav-item">
-            <a class="nav-link">CARRERAS</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link">EGRESADOS</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link">INVESTIGACIONES</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link">CONTACTO</a>
-          </li>
-        </ul>
-        </ul>
-        </ul>
-        </ul>
-
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
+          <div id="HightliteText" class="d-flex align-items-center justify-content-center" >
+               <a href="" style="text-decoration: none; color :white;" class="d-block"> Acceso </a>
+          </div>
+        </div>
+        
+       
     </div>
-  </nav>
+ </div>
+<div class="row">
+  <div class="col">
+    Column
+  </div>
+  <div class="col">
+    
+  </div>
+</div>
